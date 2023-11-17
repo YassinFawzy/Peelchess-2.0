cc493da5a60ea813e97c9bed674447cf4d674786
Yasz0 <yassinfawzycanada@gmail.com>
2023-11-17 05:33:30 +0200

    added forms registration

--- js/tournaments.js
+++ js/tournaments.js
@@ -1,5 +1,20 @@
 const modal= document.getElementById("registration");
 
+function checkInput(){
+  var email= document.getElementById("input-email").value;
+  var username= document.getElementById("input-username").value;
+
+  if (email== "" || username== ""){
+    window.alert("You must input an email and username");
+    return false;
+  }
+  else{
+    window.alert("You have successfully registered");
+    closeModal();
+    return false;
+  }
+}
+
 function showModal(){
   modal.showModal();
 }
