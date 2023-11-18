49a411e016efe8a6d1c0eef2b7f46c9bacd6042c
Yasz0 <yassinfawzycanada@gmail.com>
2023-11-18 06:53:14 +0200

    tournament registration done

--- js/tournaments.js
+++ js/tournaments.js
@@ -6,12 +6,35 @@ function checkInput(){
 
   if (email== "" || username== ""){
     window.alert("You must input an email and username");
-    return false;
+    return true;
   }
   else{
     window.alert("You have successfully registered");
     closeModal();
-    return false;
+    return true;
+  }
+}
+
+function submitForm() {
+  if (checkInput()) {
+      const form = document.getElementById('registrationForm');
+      const formData = new FormData(form);
+      const url = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfhc8slnq66wOy-4knuOSlz9bhSw80eit523v83kHXJeP8BSw/formResponse';
+
+      fetch(url, {
+          method: 'POST',
+          body: formData
+      })
+      .then(response => {
+          // Handle the response here
+          console.log(response);
+          // You can customize this part based on your needs
+          // For example, show a success message or update the UI
+      })
+      .catch(error => {
+          // Handle errors here
+          console.error(error);
+      });
   }
 }
 
